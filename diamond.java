9653ddc8c829f6b94d2f2d0f6a54f4b414edc517
saikad27 <152022750+saikad27@users.noreply.github.com>
2024-03-08 11:05:36 +0530

    Add files via upload
    User input

--- diamond.java
+++ diamond.java
@@ -1,22 +1,34 @@
+import java.util.Scanner;	//for Scanner input
+import java.io.*;		//for BufferedReader input
 class diamond{
 
-	public static void main(String[] args){
-
-		int row = 19;
+	public static void main(String[] args)throws IOException{
+		Scanner scanin = new Scanner(System.in);
+		BufferedReader buffin = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter size of the diamond(size must be odd integer) : ");
+		int row = scanin.nextInt();
+		if(row%2==0){
+			System.out.println("Invalid size");
+			System.exit(0);
+		}
 		int spaces = 2;
-		char ch1 = '$';
+		System.out.print("Enter your preferred character for the diamond or press 'd' to go with the default character: ");
+		char ch1 = (char)buffin.read();
+		if(ch1=='d' ||ch1=='D'){ch1 = '*';}		
+		buffin.skip(1);		//to skip the \n stuck in the buffer
+		System.out.print("Press any number to proceed: ");
+		int sample = buffin.read();	//this input has nothing to do with our diamond
 		char ch2 = ' ';
 		int subfactor = spaces+1;
 		int column = (row-1)*spaces+row;
-		int row_mid = (row-1)/2;
+		int row_mid = (row+1)/2;
 		int column_mid = (column-1)/2;
 		int left = column_mid;
 		int right = column_mid;
-		for(int i = 0;i <= row;i++){
+		for(int i = 1;i <= row;i++){
 				
-			
-			 
-			System.out.println();
+			if(i==1){System.out.println();}
+			System.out.print("    ");
 			for(int j = 0; j<= column; j++){
 				
 				if((i==0 || i==row)&&(left==column_mid && right==column_mid)){
@@ -24,11 +36,12 @@ class diamond{
 					if(j==column_mid) System.out.print(ch1);
 					else System.out.print(ch2);
 				}
-				else if(j%3 == 0 && j>=left &&j<=right){
+				else if(j%3==0 && j>=left && j<=right){
 					System.out.print(ch1);
 				}
 				else System.out.print(ch2);
-			} 
+			}
+			System.out.println(); 
 			if(i<row_mid){
 
                         	left = left-subfactor;
@@ -39,6 +52,7 @@ class diamond{
                                 left = left+subfactor;
                                 right = right-subfactor;
                         }
+			if(i==row){System.out.println();}
 		}
 		
 	}
