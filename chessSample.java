8acc88f7d132482e2c0bed06f74c5dedd891f7dd
saikad27 <152022750+saikad27@users.noreply.github.com>
2024-04-14 23:33:24 +0530

    Pawn movements added

--- chessSample.java
+++ chessSample.java
@@ -32,82 +32,75 @@ class chessSample{
 	static int i1 = 0;
 	static int i2 = 1;
 	static int count = 1;
+	static boolean whiteTurn = true;
+	static String blank = "[  ---  ]";
 	public static void main(String[] args){
-		Boolean userChoice = true;
-		
+		Boolean noWin = true;
+	//	String blank = "[  ---  ]";
 		String pawn = "P";
+		String white = "1";
+		String black = "0";
 		Scanner sc = new Scanner(System.in);
 		String chessPieces[][] = new String[8][8];  
-		chessPieces[0] = new String[]{"[ Rook ]","[Knight]","[Bishop]","[Queen ]","[ King ]","[Bishop]","[Knight]","[ Rook ]"};
-		chessPieces[7] = new String[]{"[ Rook ]","[Knight]","[Bishop]","[ King ]","[Queen ]","[Bishop]","[Knight]","[ Rook ]"};
+		chessPieces[0] = new String[]{"[  0R1  ]","[  0K1  ]","[  0B1  ]","[  0Q   ]","[  0K   ]","[  0B2  ]","[  0K2  ]","[  0R2  ]"};
+		chessPieces[7] = new String[]{"[  1R1  ]","[  1K1  ]","[  1B1  ]","[  1K   ]","[  1Q   ]","[  1B2  ]","[  1K2  ]","[  1R2  ]"};
 		
 		for(int i = 0; i<chessPieces.length; i++){
 			for(int j = 0; j<chessPieces[i].length; j++){
 				
-				if(i==1 || i==6){
-					chessPieces[i][j] = "[  "+pawn+(j+1)+"  ]";
+				if(i==6){
+					chessPieces[i][j] = "[  "+white+pawn+(j+1)+"  ]";
+				}
+				else if(i==1){
+					chessPieces[i][j] = "[  "+black+pawn+(j+1)+"  ]";
+				}
+				if(chessPieces[i][j]==null){
+					chessPieces[i][j] = blank;
 				}
 			}
 
 		}
-	/*	temp = chessPieces[4][3];
-		chessPieces[4][3] = chessPieces[6][3];
-		chessPieces[6][3] = temp;
-	*/
-		
+		System.out.println();
+                for(String[]row : chessPieces){
+                	for(String element : row){
+
+                                System.out.print(element+"  ");
+                        }
+                        System.out.println();
+                        System.out.println();
+                        }
 		System.out.println();		
-		for(String[]row : chessPieces){
-			for(String element : row){
-				
-				if(element==null){
-					System.out.print("[  --  ]  ");
-				}
-				else{
-					System.out.print(element+"  ");
-				}
-			}
-			System.out.println();
-			System.out.println();
-		}
-	/*	//Fetching the Pieces
-		System.out.println("Enter a pawn to move : ");
-		String name = sc.nextLine();
-			
-		int unit;
-		int i1 = 1;
-		int i2 = 2;
-		Boolean equality = false;
-		for(int i = 0; i<chessPieces.length; i++){
-			for(int j = 0; j<chessPieces[i].length; j++){
-				StringTokenizer obj = new StringTokenizer(chessPieces[i][j]," []");
-				String str = obj.nextToken(); 	
-				System.out.println(str);
-				if(str.equals(name)){
-					i1 = i;
-					i2 = j;	
-					
-					System.out.println("Success!");
-					equality = true;
-					break;
-				}
-			
-			}
-			if(equality){
-				break;
-			}
-				
-		}
-	*/
 		
 		//Moving the Pieces
-		for(int i = 0; userChoice == true; i++){	
-			Fetching(chessPieces);	
+		while(noWin){	
+			Fetching(chessPieces);
+			possibleMove(chessPieces);
+			System.out.println();
+               		for(String[]row : chessPieces){
+                        	for(String element : row){
+
+                                	System.out.print(element+"  ");
+                        	}
+                        	System.out.println();
+                        	System.out.println();
+                        }
+                System.out.println();	
 			System.out.print("Enter number of units to move : ");
 			int unit = sc.nextInt();
-			if(chessPieces[i1+unit][i2]==null){
+			if(whiteTurn){
+				unit = -unit;
+				whiteTurn = false;
+			}
+			else{
+				whiteTurn = true;
+			}
+	
+	//*** Next Operation make separate functions for pawn,rook,knight,etc and set black turn white turn. ***
+
+			if(chessPieces[i1+unit][i2]=="[   *   ]"){
 				
 				chessPieces[i1+unit][i2] = chessPieces[i1][i2];
-				chessPieces[i1][i2] = null;
+				chessPieces[i1][i2] = blank;
 				System.out.println(chessPieces[i1+unit][i2]);
 			}
 			else{
@@ -115,23 +108,18 @@ class chessSample{
 			}
 			
 			System.out.println();
-                	for(String[]row : chessPieces){
-                        	for(String element : row){
-
-                                	if(element==null){
-                                        	System.out.print("[  --  ]  ");
-                                	}
-                                	else{
-                                        	System.out.print(element+"  ");
-                                	}
+                	for(int i=0; i<chessPieces.length; i++){
+                        	for(int j=0; j<chessPieces[i].length; j++){
+					if(chessPieces[i][j]=="[   *   ]"){
+						chessPieces[i][j] = blank;
+					}
+                                        System.out.print(chessPieces[i][j]+"  ");
                         	}
                         	System.out.println();
                         	System.out.println();
                 	}
 			
-			System.out.println("Enter true to continue or false to stop : ");
-			userChoice = sc.nextBoolean();
-			if(userChoice == false){
+			if(noWin == false){
 				System.out.println("Well played!");
 			}
 			
@@ -141,24 +129,23 @@ class chessSample{
 	static void Fetching(String[][]chessPieces){
 		//Fetching the Pieces
 		Scanner input = new Scanner(System.in);
-                System.out.print("Enter a pawn to move : ");
+		if(whiteTurn){
+			System.out.println("White's turn");
+		}
+		else{
+			System.out.println("Black's turn");
+		}
+                System.out.print("Enter a piece to move : ");
                 String name = input.nextLine();
 
-                
                 Boolean equality = false;
-	/*	if(count%2==0){
-			int i = chessPieces.lenght-1;
-		}	*/
                 for(int i = 0; i<chessPieces.length; i++){
                         for(int j = 0; j<chessPieces[i].length; j++){
                                 StringTokenizer obj = new StringTokenizer(chessPieces[i][j]," []");
                                 String str = obj.nextToken();
-                             // System.out.println(str);
                                 if(str.equals(name)){
                                         i1 = i;
                                         i2 = j;
-					
-                            //          System.out.println("Success!");
                                         equality = true;
                                         break;
                                 }
@@ -169,6 +156,53 @@ class chessSample{
                         }
 
                 }
+	}
+	static void possibleMove(String[][] chessPieces){
+		int[][] arr = {};
+		String currPiece = chessPieces[i1][i2];
+		String piece = "X";
+		System.out.println("Inside possibleMove");
+		if(currPiece.contains("P")){
+			piece = "P";
+		}
+		switch(piece){
+			case "P":
+						System.out.println("Inside case (P)");
+						if(i1==6 ){
+							System.out.println("Inside if");
+							if(chessPieces[i1-1][i2]==blank && chessPieces[i1-2][i2]==blank){
+								 arr = new int[][] {{i1-1,i2},{i1-2,i2}};	
+							}
+						}
+						else if(i1==1){
+							System.out.println("Inside else if");
+							if(chessPieces[i1+1][i2]==blank && chessPieces[i1+2][i2]==blank){
+								arr = new int[][] {{i1+1,i2},{i1+2,i2}};
+							}
+						}
+				
+						else{
+							if(whiteTurn){
+								arr = new int[][] {{i1-1,i2}};
+							}
+							else{
+								arr = new int[][] {{i1+1,i2}};
+							}
+						}
+			default : 
+						System.out.println("Inside Switch");	
+		}
+		
+		for(int i=0; i<arr.length; i++){
+			for(int j=0; j<arr[i].length; ){
+				String str = chessPieces[arr[i][j++]][arr[i][j++]];
+				if(str==blank){
+					j=0;
+					chessPieces[arr[i][j++]][arr[i][j++]] = "[   *   ]";
+					
+				}
+			}
+		}
 	}
 }
 
